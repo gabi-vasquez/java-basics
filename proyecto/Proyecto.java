170e70586ce2c779797b2b2d0f0db682cf76d5b6
yersonargote <yersonargote@unicauca.edu.co>
2023-07-12 12:05:26 -0500

    Tarea: Proyecto

---
+++ proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+package proyecto;
+
+public class Proyecto {
+    // Tarea: Crear un proyecto en Java para un sistema de inventario de una tienda.
+    // Solo se guardará el nombre del producto y el precio.
+    // Al iniciar el programa debe leer el archivo 'inventario.txt' y cargar los nombres y precios de los productos en dos listas, una lista de productos y otra lista de precios.
+    // El programa debe tener un menú con las siguientes opciones.
+    // Agregar productos, donde se pide el nombre y el precio del producto.
+    // Eliminar productos, donde se pide el nombre del producto a eliminar.
+    // Buscar un producto por nombre.
+    // Mostrar todos los productos.
+    // Al finalizar el programa debe almacenar en un archivo llamado 'inventario.txt' con los nombres y precios de los productos.
+    // De la siguiente forma:
+    // nombre1, precio1
+    // nombre2, precio2
+}
